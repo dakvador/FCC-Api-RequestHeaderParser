6fcaa705115a3a6cab24be3d9a2f51f72d23aaf4
Glitch (fcc-ams-request-header) <none>
2019-10-22 20:51:30 +0000

    🕷🌎 Updated with Glitch

--- server.js
+++ server.js
@@ -1,14 +1,37 @@
+// server.js
+// where your node app starts
+
+// init project
+
 var http = require('http');
 const fetch = require('node-fetch');
+var express = require('express');
+var app = express();
 
-const server = http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  const header = JSON.stringify(req.headers);
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+app.get("/api/whoami", function (req, res) {
+    const header = JSON.stringify(req.headers);
   
-
   const language = req.headers["accept-language"];
   const software =  req.headers["user-agent"];
-
 
   const affichage = async function(language,software){
    const ip = await fetch("https://api.ipify.org?format=json") 
@@ -20,7 +43,10 @@ const server = http.createServer(function (req, res) {
   res.end();
   }
   affichage(language,software);
-})
+});
 
 
-server.listen(8080);
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
